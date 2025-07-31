7fd567cadef0c931c664fad4ccb525820ca5892e
JasonTilleyGRAD <jasontilley@utexas.edu>
2025-07-31 09:29:13 -0600

    ok

--- homework/datagen.py
+++ homework/datagen.py
@@ -14,42 +14,39 @@ def generate_dataset(output_json: str, oversample: int = 10, temperature: float 
 
     dataset = []
 
-    if os.path.exists(output_json): 
-        size = 1
-        with open(output_json, 'r') as f:
-            dataset = json.load(f)
-
-    else:
-        size = 50
-
-    model = CoTModel('HuggingFaceTb/SmolLM2-1.7B-instruct')
-    trainset = Dataset("train")
+    if not os.path.exists(output_json): 
 
 
- 
-    sampled = random.sample(list(trainset), k=min(size, len(trainset)))
+        
 
-    question, correct_answer = zip(*sampled)
+        model = CoTModel('HuggingFaceTb/SmolLM2-1.7B-instruct')
+        trainset = Dataset("train")
+        size = len(trainset)
 
-    prompts = [model.format_prompt(q) for q in question]
-    generations = model.batched_generate(prompts,num_return_sequences = 10,temperature = temperature)
-   
-    for i in range(len(question)):
-        q = question[i]
-        correct = correct_answer[i]
-        gen_list = generations[i]
-        gen_list_parsed = [model.parse_answer(g) for g in gen_list]
+    
+        sampled = random.sample(list(trainset), k=min(size, len(trainset)))
+
+        question, correct_answer = zip(*sampled)
+
+        prompts = [model.format_prompt(q) for q in question]
+        generations = model.batched_generate(prompts,num_return_sequences = 20,temperature = temperature)
+    
+        for i in range(len(question)):
+            q = question[i]
+            correct = correct_answer[i]
+            gen_list = generations[i]
+            gen_list_parsed = [model.parse_answer(g) for g in gen_list]
+                    
+            for j in range(len(gen_list)):
+                if is_answer_valid(gen_list_parsed[j], correct):
+                    dataset.append([q, correct, gen_list[j]])
+                    print(f"Success on index {i}")
+                    break
+
+
                 
-        for j in range(len(gen_list)):
-            if is_answer_valid(gen_list_parsed[j], correct):
-                dataset.append([q, correct, gen_list[j]])
-                print(f"Success on index {i}")
-                break
-
-
-               
-    with open(output_json, 'w') as f:
-        json.dump(dataset, f, indent=2)
+        with open(output_json, 'w') as f:
+            json.dump(dataset, f, indent=2)
 
 
 
